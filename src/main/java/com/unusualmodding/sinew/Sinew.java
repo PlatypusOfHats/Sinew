b85526b346011ba0c015485b5dfac7f784795f97
platypushasnohat <58866828+PlatypusOfHats@users.noreply.github.com>
2025-04-17 16:35:17 -0600

    initial setup

---
+++ src/main/java/com/unusualmodding/sinew/Sinew.java
@@ -0,0 +1,32 @@
+package com.unusualmodding.sinew;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Locale;
+
+@Mod(Sinew.MODID)
+public class Sinew {
+    public static final String MODID = "sinew";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public Sinew() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    public static ResourceLocation prefix(String name) {
+        return new ResourceLocation(MODID, name.toLowerCase(Locale.ROOT));
+    }
+}
+
