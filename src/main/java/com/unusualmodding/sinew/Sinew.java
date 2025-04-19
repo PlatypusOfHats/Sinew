c4c4f4e44b356f17dbb2fe56a116aacb7f253eff
Scouter456 <markveenstra14@gmail.com>
2025-04-19 18:34:15 +0200

    feat: add synced SavedData system with client/server support
    - Implemented ClientAccessibleSavedData and SimpleSavedData base classes using Codec serialization
    - Added automatic syncing via SyncSavedDataS2CPacket and SMessages
    - Included example data classes: WorldFlagData (client-syncable), ServerBossTrackerData (server-only)
    - Utility class ClientUtils added for Minecraft client access

--- src/main/java/com/unusualmodding/sinew/Sinew.java
+++ src/main/java/com/unusualmodding/sinew/Sinew.java
@@ -1,5 +1,6 @@
 package com.unusualmodding.sinew;
 
+import com.unusualmodding.sinew.message.SMessages;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -23,6 +24,8 @@ public class Sinew {
     }
 
     public void commonSetup(final FMLCommonSetupEvent event) {
+
+        SMessages.register();
     }
 
     public static ResourceLocation prefix(String name) {
---
+++ src/main/java/com/unusualmodding/sinew/message/SMessages.java
@@ -0,0 +1,61 @@
+package com.unusualmodding.sinew.message;
+
+import com.peeko32213.unusualprehistory.UnusualPrehistory;
+import com.peeko32213.unusualprehistory.common.message.*;
+import com.unusualmodding.sinew.Sinew;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.network.NetworkDirection;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.PacketDistributor;
+import net.minecraftforge.network.simple.SimpleChannel;
+import net.minecraftforge.server.ServerLifecycleHooks;
+
+public class SMessages {
+    private static SimpleChannel INSTANCE;
+
+    private static int packetId = 0;
+    private static int id() {
+        return packetId++;
+    }
+
+    public static void register() {
+        SimpleChannel net = NetworkRegistry.ChannelBuilder
+                .named(new ResourceLocation(Sinew.MODID, "messages"))
+                .networkProtocolVersion(() -> "1.0")
+                .clientAcceptedVersions(s -> true)
+                .serverAcceptedVersions(s -> true)
+                .simpleChannel();
+
+        INSTANCE = net;
+
+        net.registerMessage(id(), SyncSavedDataS2CPacket.class,
+                SyncSavedDataS2CPacket::encode,
+                SyncSavedDataS2CPacket::decode,
+                SyncSavedDataS2CPacket::handle);
+
+    }
+
+    public static <MSG> void sendToServer(MSG message) {
+        INSTANCE.sendToServer(message);
+    }
+
+    public static <MSG> void sendToPlayer(MSG message, ServerPlayer player) {
+        INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), message);
+    }
+
+    public static <MSG> void sendToClients(MSG message) {
+        INSTANCE.send(PacketDistributor.ALL.noArg(), message);
+    }
+
+    public static <MSG> void sendMSGToAll(MSG message) {
+        for (ServerPlayer player : ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers()) {
+            sendNonLocal(message, player);
+        }
+    }
+
+    public static <MSG> void sendNonLocal(MSG msg, ServerPlayer player) {
+        INSTANCE.sendTo(msg, player.connection.connection, NetworkDirection.PLAY_TO_CLIENT);
+    }
+}
+
